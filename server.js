039f874080c24f695d2ab9b7ad5f0097b866cbcb
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2023-04-17 22:26:40 +0900

    Initial commit
    In server.js, there is a bare-bone of express server.

---
+++ server.js
@@ -0,0 +1,9 @@
+//jshint esversion:6 //ignore warnings
+
+const express = require("express");
+const app = express(); //app- conventional word.
+
+app.listen(3000, function(){ //?
+  console.log("Server started on port 3000.");
+});
+//Now it is server
