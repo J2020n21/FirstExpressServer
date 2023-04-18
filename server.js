8df955297d75ebd722a34c3847912b6346930a22
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2023-04-18 22:15:07 +0900

    Update server.js
    Add get request: display "Hello, World"

--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 const express = require("express");
 const app = express(); //app- conventional word.
 
-app.listen(3000, function(){ //?
+//GET request
+app.get("/", function(request, response){   //to root homepage,
+    response.send("<h1>Hello, World!</h1>") //send inside of ().
+});
+
+app.listen(3000, function(){  //localhost:3000
   console.log("Server started on port 3000.");
 });
-//Now it is server
+
