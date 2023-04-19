cbbecf8a3c99ad031a5301ce01f4473c7ce9979f
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2023-04-19 23:11:46 +0900

    Add content to server.js
    Add specific descriptions.
    Understand "route". 
    Make more routes sending proper texts with its route.

--- server.js
+++ server.js
@@ -3,9 +3,26 @@
 const express = require("express");
 const app = express(); //app- conventional word.
 
-//GET request
+//"/" means; get a GETrequest from a browser-targeting this route.
+//Then respond with the callback function.
 app.get("/", function(request, response){   //to root homepage,
     response.send("<h1>Hello, World!</h1>") //send inside of ().
+});
+
+//make new request to "/contact" location.
+app.get("/contact",function(req,res){
+    res.send("Contact me: jiui0103@naver.com");
+});
+
+//It's possible to plan- setting up the code for 
+//different routes.
+
+app.get("/about",function(req,res){
+    res.send("My name is Jiui Son, from SeoulTech Univ.");
+});
+
+app.get("/hobby",function(req,res){
+    res.send("I love hanging with my friend!");
 });
 
 app.listen(3000, function(){  //localhost:3000
